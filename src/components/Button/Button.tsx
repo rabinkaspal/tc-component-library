a58bd0d29743789f8dea5d9ce93600733e5e2c26
Rabin Kaspal <rabinkaspal@gmail.com>
2025-05-27 15:46:42 +1000

    css fixes

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -2,13 +2,33 @@ import React from "react";
 import "./Button.scss";
 
 export interface ButtonProps {
+    /**
+     * Is the button a primary action button?
+     */
     primary?: boolean;
+    /**
+     * What color should the button background be?
+     */
     backgroundColor?: string;
+    /**
+     * The size of the button.
+     */
     size?: "small" | "medium" | "large";
+    /**
+     * What text should the button show?
+     */
     label: string;
+    /**
+     * Click handler for the button
+     * @returns void
+     */
     onClick?: () => void;
 }
 
+/**
+ *
+ *  Button Component for Teckcraft with all variants.
+ */
 const Button = ({
     primary = false,
     size = "medium",
--- src/components/EventCard/EventCard.tsx
+++ src/components/EventCard/EventCard.tsx
@@ -3,11 +3,41 @@ import Button from "../Button";
 import "./EventCard.scss";
 
 export interface EventProps {
+    /**
+     * Indicates if an action is required for the event.
+     * If true, an action button will be displayed.
+     */
     actionRequired?: boolean;
+    /**
+     * Specifies the type of action button to display.
+     * - "primary" for a primary action button
+     * - "secondary" for a secondary action button
+     * - If not specified, defaults to "default".
+     */
     actionType?: "primary" | "secondary" | "default";
+    /**
+     * Indicates the status of the action.
+     * This can be used to visually represent the state of the action.
+     * - "done" for completed actions
+     * - "pending" for actions that are in progress
+     * - "error" for actions that have failed
+     * - "default" for actions that have not been initiated
+     */
     actionStatus?: "done" | "pending" | "error" | "default";
+    /**
+     * The label for the action button.
+     * This will be displayed on the button when `actionRequired` is true.
+     */
     actionLabel: string;
+    /**
+     * The title of the event.
+     * This will be displayed prominently in the event card.
+     */
     eventTitle: string;
+    /**
+     * The summary of the event.
+     * This will be displayed in the event card.
+     */
     eventSummary: string;
 }
 
